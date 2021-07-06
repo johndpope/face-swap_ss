c94f48b1b35cc94adb4130fbfff089d56075a8de
detkov <nds-gfk@mail.ru>
2021-07-06 16:58:04 +0000

    Removed MoviePy, changed to FFMPEG

--- util/videoswap.py
+++ util/videoswap.py
@@ -2,16 +2,27 @@ import os
 import shutil
 import subprocess
 from datetime import datetime
-from glob import glob
-from os.path import basename, dirname, exists, isfile, join, splitext
+from os.path import basename, dirname, exists, isfile, join, normpath, splitext
+from time import gmtime, perf_counter, strftime
 
 import cv2
 import numpy as np
 import torch
-from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
 from tqdm import tqdm
 
 from util.reverse2original import reverse2wholeimage
+
+
+def timer(func_name):
+    def actual_decorator(func):
+        def wrapper(*args, **kwargs):
+            script_time_start = perf_counter()
+            result = func(*args, **kwargs)
+            elapsed = strftime("%H:%M:%S", gmtime(perf_counter() - script_time_start))
+            print(f'> Time elapsed on `{func_name}`: {elapsed} ({datetime.now()}).')
+            return result
+        return wrapper
+    return actual_decorator
 
 
 def execute_command(command: str, error_message: str,
@@ -25,19 +36,24 @@ def execute_command(command: str, error_message: str,
             raise Exception(error_message)
     return exitcode
 
+@timer('Extracting audio')
 def extract_audio(video_path: str, audio_path: str) -> None:
-    print(f'=> Extracting audio from video: "{basename(video_path)}"...')
+    print(f'=> Extracting audio from video "{basename(video_path)}"...')
     command = f'ffmpeg -hide_banner -loglevel error -i "{video_path}" -vn -ar 44100 -ac 2 -ab 192K -f wav -y "{audio_path}"'
-    execute_command(command, f'##### Unable to extract audio of {video_path} ({datetime.now()}). #####', print_on_error=True)
+    execute_command(command, f'> > > > > Unable to extract audio of {video_path} ({datetime.now()}).', print_on_error=True)
 
-def insert_audio(save_path, audio_path):
+@timer('Creating video')
+def create_video(save_path: str, audio_path: str, frames_path: str, fps: float) -> str:
+    print(f'=> Creating video from frames at "{frames_path}"...')
+    os.makedirs(dirname(save_path), exist_ok=True)
     if isfile(audio_path):
-        temp_path = splitext(save_path)[0] + "_TEMP" + splitext(save_path)[1]
-        os.rename(save_path, temp_path)
-        command = f'ffmpeg -hide_banner -loglevel warning -i "{temp_path}" -i "{audio_path}" -c:v libx264 -map 0:v:0 -map 1:a:0 -c:a aac -b:a 192k -pix_fmt yuv420p "{save_path}" -y'
-        execute_command(command, f'Error while creating the video from the video {temp_path} and audio from {audio_path} ({datetime.now()}).', 
+        command = f'ffmpeg -hide_banner -loglevel warning -pattern_type glob -r "{fps}" -i "{normpath(frames_path)}/*.jpg" -i "{audio_path}" -c:v libx264 -pix_fmt yuv420p -y "{save_path}"'
+        execute_command(command, f'> > > > > Error while creating the video from the frames of {frames_path} and audio from {audio_path} ({datetime.now()}).', 
                         raise_on_error=True)
-        os.remove(temp_path)
+    else:
+        command = f'ffmpeg -hide_banner -loglevel warning -pattern_type glob -r "{fps}" -i "{normpath(frames_path)}/*.jpg" -c:v libx264 -pix_fmt yuv420p -y "{save_path}"'
+        execute_command(command, f'> > > > > Error while creating the video from the frames of {frames_path} ({datetime.now()}).', 
+                        raise_on_error=True)
 
 def get_frames_n(video_path: str) -> int:
     def _manual_count(handler):
@@ -60,7 +76,10 @@ def _totensor(array):
     img = tensor.transpose(0, 1).transpose(0, 2).contiguous()
     return img.float().div(255)
 
+
+@timer('Swapping Face')
 def video_swap(video_path, id_veсtor, swap_model, detect_model, save_path, temp_results_dir='./temp_results', crop_size=224):
+    print(f'=> Swapping face in "{video_path}"...')
     if exists(temp_results_dir):
         shutil.rmtree(temp_results_dir)
     os.makedirs(temp_results_dir)
@@ -93,11 +112,5 @@ def video_swap(video_path, id_veсtor, swap_model, detect_model, save_path, temp
             cv2.imwrite(join(temp_results_dir, 'frame_{:0>7d}.jpg'.format(frame_index)), frame)
 
     video.release()
-
-    image_filenames = sorted(glob(join(temp_results_dir, '*.jpg')))
-
-    os.makedirs(dirname(save_path), exist_ok=True)
-    clips = ImageSequenceClip(image_filenames, fps=fps)
-    clips.write_videofile(save_path)
-    insert_audio(save_path, audio_path)
+    create_video(save_path, audio_path, temp_results_dir, fps)
     shutil.rmtree(temp_results_dir)
